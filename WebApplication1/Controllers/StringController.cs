1c11a62963c923171a23724c730ed0af6888a7e5
Andrew-Essam <andrew.essam55@yahoo.com>
2018-11-03 11:23:32 +0200

    Upload Files

---
+++ WebApplication1/Controllers/StringController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class StringController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public String Andrew()
+        {
+            return "Hello Andrew";
+        }
+
+        public String Task()
+        {
+            return "Hello ASP.NET CORE";
+        }
+
+    }
+}
